a93cb2d6130905db29beb4fe623fe86303e6301f
bkilian <kilian.boudeaud@ynov.com>
2026-05-22 11:45:45 +0200

    [ADD] fonction checkPassword test MP  et  hashPassword encriptage MP

--- security/password.go
+++ security/password.go
@@ -1,1 +1,27 @@
 package security
+
+import (
+    "fmt"
+    "golang.org/x/crypto/bcrypt"
+)
+
+func hashPassword(password string) (string, error) {
+    hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+    return string(hash), err
+}
+
+func checkPassword(testpassword, truepassword string) bool {
+    hash, _ := hashPassword(truepassword)
+   
+	testPassword, _ := hashPassword(testpassword)
+
+	if testPassword == hash {
+		fmt.Println("Mot de passe correct")
+		return true
+	} else {
+		fmt.Println("Mot de passe incorrect")
+		return false
+	}
+}
+
+// ajouter "golang.org/x/crypto/bcrypt" dans le go.mod
