547a70fabefc2bc950aa652209bc4412e0192868
lgabor <gabor.lignier@ynov.com>
2026-06-02 09:24:03 +0200

    [FIX] hash password

--- security/password.go
+++ security/password.go
@@ -1,1 +1,35 @@
 package security
+
+import (
+    "fmt"
+    "golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+    hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+    return string(hash), err
+}
+
+func hashPassword(password string) (string, error) {
+    hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+    return string(hash), err
+}
+
+func CheckPassword(testpassword, hashedPassword string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(testpassword))
+    return err == nil
+}
+
+func checkPassword(testpassword, truepassword string) bool {
+    hash, _ := hashPassword(truepassword)
+   
+	testPassword, _ := hashPassword(testpassword)
+
+	if testPassword == hash {
+		fmt.Println("Mot de passe correct")
+		return true
+	} else {
+		fmt.Println("Mot de passe incorrect")
+		return false
+	}
+}
