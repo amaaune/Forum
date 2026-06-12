342f82c8e13c041196cccd77dbd8ecc0a87db3d8
aamaury <amaury.aune@ynov.com>
2026-06-12 08:29:11 +0200

    [ADD] ajout models pour gestion content

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"forum/database"
 	"forum/handlers"
 	"forum/middleware"
+	"forum/models"
 	"html/template"
 	"log"
 	"net/http"
 )
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
+func renderTemplate(w http.ResponseWriter, tmpl string, data []models.Post) {
 	t, err := template.ParseFiles("templates/"+tmpl, "templates/poly/header.html",
 		"templates/poly/footer.html")
 	if err != nil {
@@ -17,26 +18,26 @@ func renderTemplate(w http.ResponseWriter, tmpl string) {
 		return
 	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if err := t.Execute(w, nil); err != nil {
+	if err := t.Execute(w, data); err != nil {
 		http.Error(w, "Erreur execution template: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "login.html")
+	renderTemplate(w, "login.html", nil)
 }
 
 func errorP(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "error.html")
+	renderTemplate(w, "error.html", nil)
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "register.html")
+	renderTemplate(w, "register.html", nil)
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "post.html")
+	renderTemplate(w, "post.html", nil)
 }
 
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
@@ -52,14 +53,18 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		renderTemplate(w, "index.html")
+		posts, err := handlers.GetPosts()
+		if err != nil {
+			return
+		}
+		renderTemplate(w, "index.html", posts)
 	})
-	http.HandleFunc("/login",    login)
-	http.HandleFunc("/error",    errorP)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/error", errorP)
 	http.HandleFunc("/register", register)
 
 	// ✅ post() est réutilisée ici
-	http.HandleFunc("/post",     middleware.RequireAuth(post))
+	http.HandleFunc("/post", middleware.RequireAuth(post))
 
 	// ✅ category() remplacée par handlers.CategoryHandler
 	http.HandleFunc("/category", middleware.RequireAuth(handlers.CategoryHandler))
