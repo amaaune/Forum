63b36ac78bfe19e3ce280cd2c17539793c6d9a55
lgabor <gabor.lignier@ynov.com>
2026-06-12 16:44:15 +0200

    [ADD] func LogAccount

--- handlers/login.go
+++ handlers/login.go
@@ -1,58 +1,24 @@
-package users
+package handlers
 
 import (
 	"database/sql"
 	"errors"
-	"regexp"
-	"strings"
+
+	"forum/auth"
 )
 
-func LogAccount(db *sql.DB, email string, username string, password string) (bool, error) {
+func LogAccount(db *sql.DB, email string, password string) (bool, error) {
 
-	
-	if strings.TrimSpace(email) == "" {
-		return false, errors.New("email obligatoire")
-	}
-
-	emailRegex := `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
-	if !regexp.MustCompile(emailRegex).MatchString(email) {
-		return false, errors.New("email invalide")
-	}
-
-
-	if strings.TrimSpace(username) == "" {
-		return false, errors.New("nom d'utilisateur obligatoire")
-	}
-
-	if len(username) < 3 || len(username) > 30 {
-		return false, errors.New("le nom d'utilisateur doit contenir entre 3 et 30 caractères")
-	}
-
-	usernameRegex := `^[a-zA-Z0-9_]+$`
-	if !regexp.MustCompile(usernameRegex).MatchString(username) {
-		return false, errors.New("nom d'utilisateur invalide (lettres, chiffres et _ uniquement)")
-	}
-
-	if len(password) < 7 {
-		return false, errors.New("le mot de passe doit contenir au moins 8 caractères")
-	}
-
-	hasUpper := regexp.MustCompile(`[A-Z]`).MatchString(password)
-	hasLower := regexp.MustCompile(`[a-z]`).MatchString(password)
-	hasNumber := regexp.MustCompile(`[0-9]`).MatchString(password)
-
-	if !hasUpper || !hasLower || !hasNumber {
-		return false, errors.New("le mot de passe doit contenir une majuscule, une minuscule et un chiffre")
-	}
-
-	query := `
-		SELECT * FROM users
-		WHERE email = ? AND password = ?
-	`
-
-	_, err := db.Query(query, email, password)
+	user, err := auth.GetUserByEmail(db, email)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, errors.New("utilisateur introuvable")
+		}
 		return false, err
+	}
+
+	if user.Password != password {
+		return false, errors.New("mot de passe incorrect")
 	}
 
 	return true, nil
